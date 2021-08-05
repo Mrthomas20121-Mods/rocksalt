be836971fd2b018551a554b5fe16dde915713537
mrthomas20121 <thomas77300@gmail.com>
2021-08-05 16:47:57 +0200

    Update RockSalt.java

--- src/main/java/mrthomas20121/rocksalt/RockSalt.java
+++ src/main/java/mrthomas20121/rocksalt/RockSalt.java
@@ -8,12 +8,12 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
 @Mod(modid = RockSalt.MODID, name = RockSalt.NAME, version = RockSalt.VERSION,
-        dependencies = "required-after:forge@[14.23.5.2847,);"
-                + "required-after:tfc@[1.7.2.160,);")
+        dependencies = "required-after:forge@[14.23.5.2855,);"
+                + "required-after:tfc@[1.7.21.179,);")
 public class RockSalt
 {
     public static final String MODID = "rocksalt";
-    public static final String NAME = "rocksalt";
+    public static final String NAME = "Rocksalt";
     public static final String VERSION = "1.0.3";
 
     private static Logger logger;
